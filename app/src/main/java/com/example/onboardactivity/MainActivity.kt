efe65fcf748874ceeadffecfe151dd07eb7163e1
Your Name <you@example.com>
2024-10-22 19:30:05 +0300

    [STABLE]

--- app/src/main/java/com/example/onboardactivity/MainActivity.kt
+++ app/src/main/java/com/example/onboardactivity/MainActivity.kt
@@ -13,20 +13,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
-
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        setupActionBarWithNavController(navController)
-    }
-
-    override fun onSupportNavigateUp(): Boolean {
-        val navController = supportFragmentManager.findFragmentById(R.id.nav_host_fragment)
-            ?.findNavController()
-        return navController?.navigateUp() ?: false || super.onSupportNavigateUp()
     }
 }
